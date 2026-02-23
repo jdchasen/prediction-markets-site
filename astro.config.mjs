f55e68ce1d6ed0499d5f91c0fa8700afe6020a0c
J.D. Chasen <jdch227@uky.edu>
2026-02-22 22:31:10 -0600

    Update site URL to masterpredictionmarkets.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://predictionmarketpro.com',
+  site: 'https://masterpredictionmarkets.com',
   integrations: [
     tailwind(),
     sitemap(),
